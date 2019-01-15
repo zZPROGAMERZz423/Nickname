3e196c07cbaac7d98f5dc1b4522006a43fafeabf
zZPROGAMERZz423 <40534566+zZPROGAMERZz423@users.noreply.github.com>
2019-01-15 23:43:53 +0800

    Add files via upload

--- src/Nickname/Main.php
+++ src/Nickname/Main.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Nickname;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
+use Nickname\NickCommand;
+use Nickname\Main;
+
+class Main extends PluginBase implements Listener { 
+
+public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getCommandMap()->register("nick", new NickCommand($this));
+		$this->getLogger()->notice("Nickname has been successfully enabled by the server");
+		}
+		
+public function onDisable(){
+        $this->getLogger()->notice("Nickname has been disabled by the server");
+        }
+}
+		
+		
