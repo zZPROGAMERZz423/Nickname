830e72c562485dfc3c0e4f358f47971c44eb6af5
zZPROGAMERZz423 <40534566+zZPROGAMERZz423@users.noreply.github.com>
2019-01-16 23:24:37 +0800

    Issue Recoded

--- src/zZPROGAMERZz423/Main.php
+++ src/zZPROGAMERZz423/Main.php
@@ -1,20 +1,5 @@
 <?php
-# [Plugin] by zZPROGAMERZz423
-/**
- * Copyright 2018 zZPROGAMERZz423
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 namespace zZPROGAMERZz423\Nickname;
 
 use pocketmine\plugin\PluginBase;
@@ -32,7 +17,7 @@ class Main extends PluginBase implements Listener {
 public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getServer()->getCommandMap()->register("nick", new NickCommand($this));
-		$this->getLogger()->notice("Nickname has been successfully enabled by the server");
+		$this->getLogger()->info("Nickname has been successfully enabled by the server");
 		}
 		
 public function onDisable(){
